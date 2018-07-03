91baea684ab7cbcc184d7f12ce2c57ba9db6b393
Greg <gchristian@gc-master.cyclotron.tamu.edu>
2018-07-03 09:35:39 -0500

    IOndentation

--- include/Event.hpp
+++ include/Event.hpp
@@ -7,40 +7,40 @@
 
 namespace compass_unpack {
   
-  class InputFileBin;
+class InputFileBin;
   
-  class Event {
-  public:
-    Event();
-    ~Event();
+class Event {
+public:
+	Event();
+	~Event();
 		
-    UShort_t  GetBoard()       const { return fBoard;       }
-    UShort_t  GetChannel()     const { return fChannel;     }
-    UShort_t  GetEnergy()      const { return fEnergy;      }
-    UShort_t  GetEnergyShort() const { return fEnergyShort; }
-    Long64_t  GetTimestamp()   const { return fTimestamp;   }
-    UInt_t    GetFlags()       const { return fFlags;       }
-    const std::vector<UShort_t>& GetWaveform() const { return fWaveform; }
+	UShort_t  GetBoard()       const { return fBoard;       }
+	UShort_t  GetChannel()     const { return fChannel;     }
+	UShort_t  GetEnergy()      const { return fEnergy;      }
+	UShort_t  GetEnergyShort() const { return fEnergyShort; }
+	Long64_t  GetTimestamp()   const { return fTimestamp;   }
+	UInt_t    GetFlags()       const { return fFlags;       }
+	const std::vector<UShort_t>& GetWaveform() const { return fWaveform; }
 		
-    bool operator< (const Event& rhs) const;
+	bool operator< (const Event& rhs) const;
 		
- private:
-    UShort_t  fBoard;       
-    UShort_t  fChannel;
-    UShort_t  fEnergy;
-    UShort_t  fEnergyShort;
-    Long64_t  fTimestamp;
-    UInt_t    fFlags;
-    std::vector<UShort_t> fWaveform;
+private:
+	UShort_t  fBoard;       
+	UShort_t  fChannel;
+	UShort_t  fEnergy;
+	UShort_t  fEnergyShort;
+	Long64_t  fTimestamp;
+	UInt_t    fFlags;
+	std::vector<UShort_t> fWaveform;
 
- public:
-		static void SetMatchWindow(Long64_t Window) { kMatchWindow = Window; }
-    static Long64_t GetMatchWindow() { return kMatchWindow;   } 
- private:
-    static std::atomic<Long64_t> kMatchWindow;
+public:
+	static void SetMatchWindow(Long64_t Window) { kMatchWindow = Window; }
+	static Long64_t GetMatchWindow() { return kMatchWindow;   } 
+private:
+	static std::atomic<Long64_t> kMatchWindow;
 
-		friend class compass_unpack::InputFileBin;
-	};
+	friend class compass_unpack::InputFileBin;
+};
   
 }
 
