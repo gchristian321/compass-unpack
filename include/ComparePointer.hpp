128cd06c478fc0d9cb29d9faa6fe7c54e1bc0a1b
Greg <gchristian@gc-master.cyclotron.tamu.edu>
2018-07-12 13:43:09 -0500

    Made special class for comparing pointers

---
+++ include/ComparePointer.hpp
@@ -0,0 +1,26 @@
+#ifndef CU_COMPARE_POINTER
+#define CU_COMPARE_POINTER
+
+namespace compass_unpack {
+
+template <class T> class ComparePointer {
+public:
+	bool operator()(const T* const l, const T* const r)
+		{ return *l < *r; }
+};
+
+template <class T> class CompareUniquePointer {
+public:
+	bool operator()(const std::unique_ptr<T>& l, const std::unique_ptr<T>& r)
+		{ return *l < *r; }
+};
+
+template <class T> class CompareSharedPointer {
+public:
+	bool operator()(const std::shared_ptr<T>& l, const std::shared_ptr<T>& r)
+		{ return *l < *r; }
+};
+
+}
+
+#endif
