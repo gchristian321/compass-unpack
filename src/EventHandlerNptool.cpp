6f04e3e0c9a19a60399d6aed1798e60207000e94
Greg <gchristian@gc-master.cyclotron.tamu.edu>
2018-07-14 06:51:48 -0500

    Made parsing code more clear

--- src/EventHandlerNptool.cpp
+++ src/EventHandlerNptool.cpp
@@ -13,10 +13,10 @@ using namespace std;
 namespace cu = compass_unpack;
 
 cu::EventHandlerNptool::EventHandlerNptool
-(const std::string& outputFileName, 
- const std::string& outputTreeName, 
- const std::string& outputTreeTitle,
- const std::string& detectorConfigFile) :
+(const string& outputFileName, 
+ const string& outputTreeName, 
+ const string& outputTreeTitle,
+ const string& detectorConfigFile) :
 	fFilename(outputFileName),
 	fTreename(outputTreeName),
 	fTreetitle(outputTreeTitle),
@@ -51,18 +51,18 @@ void cu::EventHandlerNptool::BeginningOfEvent()
 }
 
 Long64_t cu::EventHandlerNptool::HandleEvent
-(Long64_t eventNo, const std::vector<std::shared_ptr<Event> >& matches)
+(Long64_t eventNo, const vector<shared_ptr<Event> >& matches)
 {
 	fEventNo = eventNo;
 	for(auto& event : matches) {
-		auto match = fFpdMap.find(std::make_pair(event->fBoard, event->fChannel));
+		auto match = fFpdMap.find(make_pair(event->fBoard, event->fChannel));
 		if(match != fFpdMap.end()) {
 			auto& SetEnergy = match->second.first;
 			auto& SetTime = match->second.second;
 			SetEnergy(event->fEnergy);
 			SetTime(event->fTimestamp);
 		} else {
-			std::cerr << "WARNING: channel map not found for (board, channel): (" << event->fBoard << ", " << event->fChannel << ")\n";
+			cerr << "WARNING: channel map not found for (board, channel): (" << event->fBoard << ", " << event->fChannel << ")\n";
 		}
 	}
 }
@@ -91,80 +91,72 @@ void cu::EventHandlerNptool::ParseConfig()
     exit(1);
   }
 	
-  string key,buffer, detector,token;
+  string key,buffer,detector,token;
   int value,channel,module;
-  // Reading setting line
-  infile >> key; 
-  while(key!="MAP"){
-    if(key == "ADC"){
-      infile >> token >> value;
-    } 
-    else if(key == "TDC"){
-      infile >> token >> value;
-    }
-		else if(key.compare(0,1,"%")==0){
-		 infile.ignore(std::numeric_limits<std::streamsize>::max(), '\n')  ;
-	 }
-	 else{
-		 cout << "**** INFO : Token unknown : " << token << " ****" << endl;
-	 }
-    infile >> key ;
-  }
-
-	auto Tokenize = [](const std::string& token, UShort_t& row, UShort_t& col) {
-		std::unique_ptr<TObjArray> tok(TString(token.c_str()).Tokenize("_"));
+  // Ignore everything until "MAP"
+  do {
+		infile >> key; 
+		infile.ignore(numeric_limits<streamsize>::max(), '\n');
+  } while(key!="MAP");
+	
+	auto Tokenize = [](const string& token, string& component, UShort_t& row, UShort_t& col) {
+		unique_ptr<TObjArray> tok(TString(token.c_str()).Tokenize("_"));
 		if(tok->GetEntries() == 4) {
+			component = ((TObjString*)tok->At(0))->String();
 			row = atoi(string(((TObjString*)tok->At(1))->String()).substr(1).c_str());
 			col = atoi(string(((TObjString*)tok->At(2))->String()).substr(1).c_str());
 			return true;
 		}
 		return false;
 	};
-	
-  while(infile >> key >> module >> buffer >> channel >> detector >> token){
-    if(key.compare(0,1,"%")!=0){
-      if(key == "ADC") {
-				if(detector == "FpdTAMU") {
-					UShort_t row, col;
-					if(Tokenize(token, row, col)) {
-						std::string component = token.substr(0, token.find("_"));
-						//
-						if(component == "AWIRE") {
-							fFpdMap.emplace
-								(std::make_pair(module, channel),
-								 std::make_pair(std::bind(&TFPDTamuData::Set_AWire_E, fFpd, row, col, std::placeholders::_1),
-																std::bind(&TFPDTamuData::Set_AWire_T, fFpd, row, col, std::placeholders::_1)
-									 ) );
-						}
-						else if(component.substr(0, component.size()-1) == "MICRO") {
-							UShort_t det = atoi(component.substr(5).c_str());
-							fFpdMap.emplace
-								(std::make_pair(module, channel),
-								 std::make_pair(std::bind(&TFPDTamuData::Set_Micro_E, fFpd, det, row, col, std::placeholders::_1),
-																std::bind(&TFPDTamuData::Set_Micro_T, fFpd, det, row, col, std::placeholders::_1)
-									 ) );
-						}
-						else if(component == "DELTA") {
-							fFpdMap.emplace
-								(std::make_pair(module, channel),
-								 std::make_pair(std::bind(&TFPDTamuData::Set_Delta_E, fFpd, row, std::placeholders::_1),
-																std::bind(&TFPDTamuData::Set_Delta_T, fFpd, row, std::placeholders::_1)
-									 ) );
-						}																				
-						else if(component == "PLAST") {
-							fFpdMap.emplace
-								(std::make_pair(module, channel),
-								 std::make_pair(std::bind(&TFPDTamuData::Set_Plast_E, fFpd, col, std::placeholders::_1),
-																std::bind(&TFPDTamuData::Set_Plast_T, fFpd, col, std::placeholders::_1)
-									 ) );
-						}																				
-						else {
-							std::cerr << "WARNING: unrecognized FPDTAMU component: \"" << component << "\", skipping...\n";
-						}
+
+	UShort_t row, col;
+	string line, component;
+  while(std::getline(infile, line)) {
+		// Ignore comments and parse into tokens
+		istringstream iss(line.substr(0, line.find("%")));
+		iss >> key >> module >> buffer >> channel >> detector >> token;
+
+		// Process ADC keys to map
+		if(key == "ADC") {
+			if(detector == "FpdTAMU") {
+				if(Tokenize(token, component, row, col)) {
+					//
+					if(component == "AWIRE") {
+						fFpdMap.emplace
+							(make_pair(module, channel),
+							 make_pair(bind(&TFPDTamuData::Set_AWire_E, fFpd, row, col, placeholders::_1),
+												 bind(&TFPDTamuData::Set_AWire_T, fFpd, row, col, placeholders::_1)
+								 ) );
 					}
+					else if(component.substr(0, component.size()-1) == "MICRO") {
+						UShort_t det = atoi(component.substr(5).c_str());
+						fFpdMap.emplace
+							(make_pair(module, channel),
+							 make_pair(bind(&TFPDTamuData::Set_Micro_E, fFpd, det, row, col, placeholders::_1),
+												 bind(&TFPDTamuData::Set_Micro_T, fFpd, det, row, col, placeholders::_1)
+								 ) );
+					}
+					else if(component == "DELTA") {
+						fFpdMap.emplace
+							(make_pair(module, channel),
+							 make_pair(bind(&TFPDTamuData::Set_Delta_E, fFpd, row, placeholders::_1),
+												 bind(&TFPDTamuData::Set_Delta_T, fFpd, row, placeholders::_1)
+								 ) );
+					}																				
+					else if(component == "PLAST") {
+						fFpdMap.emplace
+							(make_pair(module, channel),
+							 make_pair(bind(&TFPDTamuData::Set_Plast_E, fFpd, col, placeholders::_1),
+												 bind(&TFPDTamuData::Set_Plast_T, fFpd, col, placeholders::_1)
+								 ) );
+					}																				
 					else {
-						std::cerr << "WARNING: unecognized detector: \"" << detector << "\", skipping...\n";
+						cerr << "WARNING: unrecognized FPDTAMU component: \"" << component << "\", skipping...\n";
 					}
+				}
+				else {
+					cerr << "WARNING: unecognized detector: \"" << detector << "\", skipping...\n";
 				}
 			}
 		}
