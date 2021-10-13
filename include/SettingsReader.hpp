4b2ccd3eaeef98c3711589a9b6403e891e7cd91a
Greg Christian <gchristian@tamu.edu>
2021-10-13 12:21:23 -0500

    Settings reader now handles multiple boards, and has functions to search for things we need

--- include/SettingsReader.hpp
+++ include/SettingsReader.hpp
@@ -1,5 +1,6 @@
 #ifndef CUNPACK_SETTINGS_READER_HEADER
 #define CUNPACK_SETTINGS_READER_HEADER
+#include <vector>
 #include <TString.h>
 #include <TXMLEngine.h>
 
@@ -10,6 +11,16 @@ public:
 	SettingsReader(const TString& runDir);
 	~SettingsReader();
 	TString GetNodeContent(const TString& path);
+	TString GetAcquisitionMode();
+	TString GetFileFormat();
+	std::vector<TString> GetBoardLabels();
+	std::vector<TString> GetBoardDPPTypes();
+	
+private:
+	XMLNodePointer_t GetAcquisitionMementoNode();
+	std::vector<XMLNodePointer_t> GetBoardNodes();
+	TString GetContentFromAcqMemento(const TString& nodename);
+	std::vector<TString> GetContentFromBoard(const TString& nodename);
 	
 private:
 	TXMLEngine fXML;
--- src/SettingsReader.cpp
+++ src/SettingsReader.cpp
@@ -1,3 +1,4 @@
+#include <iostream>
 #include <sstream>
 #include <memory>
 #include <TObjArray.h>
@@ -22,6 +23,127 @@ cu::SettingsReader::SettingsReader(const TString& runDir):
 cu::SettingsReader::~SettingsReader()
 {
 	fXML.FreeDoc(fXMLDoc);
+}
+
+
+TString cu::SettingsReader::GetContentFromAcqMemento(const TString& nodename)
+{
+	XMLNodePointer_t acqNode = GetAcquisitionMementoNode();
+	XMLNodePointer_t child = 0;
+	if(acqNode) {
+		child = fXML.GetChild(acqNode);
+		while(child != 0) {
+			if(TString(fXML.GetNodeName(child)) == nodename) {
+				break;
+			}
+			child = fXML.GetNext(child);
+		}
+	}
+	else {
+		throw invalid_argument("Coundn't find \"acquisitionMemento\" node!");
+	}
+
+	if(!child) {
+		stringstream err;
+		err << "Couldn't find \"acquisitionMemento/" << nodename << "\" node!";
+		throw invalid_argument(err.str().c_str());
+	}
+	const char* content = fXML.GetNodeContent(child);
+	if (content == 0) {
+		stringstream err;
+		err << "No content for \"acquisitionMemento/" << nodename << "\" node!";
+		throw invalid_argument(err.str().c_str());
+	}
+	return TString(content);
+}
+
+
+TString cu::SettingsReader::GetAcquisitionMode()
+{
+	return GetContentFromAcqMemento("acquisitionMode");
+}
+
+TString cu::SettingsReader::GetFileFormat()
+{
+	return GetContentFromAcqMemento("fileFormat");
+}
+
+vector<TString> cu::SettingsReader::GetContentFromBoard(const TString& nodename)
+{
+	vector<XMLNodePointer_t> boardNodes = GetBoardNodes();
+	vector<XMLNodePointer_t> children;
+	if(boardNodes.size()) {
+		XMLNodePointer_t child = 0;
+		for(auto bNode : boardNodes) {
+			child = fXML.GetChild(bNode);
+			while(child != 0) {
+				if(TString(fXML.GetNodeName(child)) == nodename) {
+					children.push_back(child);
+					break;
+				}
+				child = fXML.GetNext(child);
+			}
+			if(!child) {
+				stringstream err;
+				err << "Couldn't find \"board/" << nodename << "\" nodes!";
+				throw invalid_argument(err.str().c_str());
+			}
+		}
+	}
+	else {
+		throw invalid_argument("Coundn't find any \"board\" nodes!");
+	}
+
+	vector<TString> retvals;
+	for(auto child : children) {
+		const char* content = fXML.GetNodeContent(child);
+		if (content == 0) {
+			stringstream err;
+			err << "No content for \"board/" << nodename << "\" nodes!";
+			throw invalid_argument(err.str().c_str());
+		}
+		retvals.push_back(content);
+	}
+	return retvals;
+}
+
+vector<TString> cu::SettingsReader::GetBoardLabels()
+{
+	return GetContentFromBoard("label");
+}
+
+vector<TString> cu::SettingsReader::GetBoardDPPTypes()
+{
+	return GetContentFromBoard("dppType");
+}
+
+XMLNodePointer_t
+cu::SettingsReader::GetAcquisitionMementoNode()
+{
+	XMLNodePointer_t mainnode = fXML.DocGetRootElement(fXMLDoc);
+	XMLNodePointer_t child = fXML.GetChild(mainnode);
+	while(child != 0) {
+		if(TString(fXML.GetNodeName(child)) == "acquisitionMemento") {
+			return child;
+		}
+		child = fXML.GetNext(child);
+	}
+	return 0;
+}
+
+vector<XMLNodePointer_t>
+cu::SettingsReader::GetBoardNodes()
+{
+	vector<XMLNodePointer_t> boardNodes;
+	XMLNodePointer_t mainnode = fXML.DocGetRootElement(fXMLDoc);
+	XMLNodePointer_t child = fXML.GetChild(mainnode);
+	while(child != 0) {
+		if(TString(fXML.GetNodeName(child)) == "board") {
+			boardNodes.push_back(child);
+		}
+		child = fXML.GetNext(child);
+	}
+	return boardNodes;
 }
 
 TString cu::SettingsReader::GetNodeContent
